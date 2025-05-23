168ef89dfd876266a7bf6a4474f34d264a504683
Guillermo Bescos <g.bescos@yahoo.com>
2025-05-23 20:10:36 +0100

    go

--- app/src/idl/delegation_demo.ts
+++ app/src/idl/delegation_demo.ts
@@ -2,28 +2,28 @@
  * Program IDL in camelCase format in order to be used in JS/TS.
  *
  * Note that this is only a type helper and is not the actual IDL. The original
- * IDL can be found at `target/idl/counter.json`.
+ * IDL can be found at `target/idl/delegation_demo.json`.
  */
-export type Counter = {
-  "address": "9k76vmCCxihurhKTtgQ8unmjpm3CgZCkfr32Dpp2dmKm",
+export type DelegationDemo = {
+  "address": "5QdNueoih49C6pmYCaUvX5TN2Sar47FQkGXKMpt5HmHg",
   "metadata": {
-    "name": "counter",
+    "name": "delegationDemo",
     "version": "0.1.0",
     "spec": "0.1.0",
     "description": "Created with Anchor"
   },
   "instructions": [
     {
-      "name": "increment",
+      "name": "setDelegation",
       "discriminator": [
-        11,
-        18,
-        104,
-        9,
-        104,
-        174,
-        59,
-        33
+        28,
+        231,
+        56,
+        197,
+        225,
+        173,
+        129,
+        74
       ],
       "accounts": [
         {
@@ -32,21 +32,12 @@ export type Counter = {
           "signer": true
         },
         {
-          "name": "agent",
-          "signer": true
+          "name": "delegator"
         },
         {
-          "name": "counter",
+          "name": "delegation",
           "writable": true,
-          "pda": {
-            "seeds": [
-              {
-                "kind": "account",
-                "path": "agent.delegator",
-                "account": "delegation"
-              }
-            ]
-          }
+          "signer": true
         },
         {
           "name": "systemProgram",
@@ -54,22 +45,45 @@ export type Counter = {
         }
       ],
       "args": []
+    },
+    {
+      "name": "transact",
+      "discriminator": [
+        217,
+        149,
+        130,
+        143,
+        221,
+        52,
+        252,
+        119
+      ],
+      "accounts": [
+        {
+          "name": "agent",
+          "signer": true
+        },
+        {
+          "name": "delegation",
+          "pda": {
+            "seeds": [
+              {
+                "kind": "account",
+                "path": "agent"
+              }
+            ]
+          }
+        }
+      ],
+      "args": [
+        {
+          "name": "data",
+          "type": "bytes"
+        }
+      ]
     }
   ],
   "accounts": [
-    {
-      "name": "counter",
-      "discriminator": [
-        255,
-        176,
-        4,
-        245,
-        188,
-        253,
-        124,
-        25
-      ]
-    },
     {
       "name": "delegation",
       "discriminator": [
@@ -85,22 +99,6 @@ export type Counter = {
     }
   ],
   "types": [
-    {
-      "name": "counter",
-      "type": {
-        "kind": "struct",
-        "fields": [
-          {
-            "name": "owner",
-            "type": "pubkey"
-          },
-          {
-            "name": "counter",
-            "type": "u64"
-          }
-        ]
-      }
-    },
     {
       "name": "delegation",
       "type": {
